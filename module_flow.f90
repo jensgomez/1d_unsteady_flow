e56fb8e586f01e2f57d919484c207e8992188768
jens <jerald.daniel@gmail.com>
2018-10-24 22:46:32 -0400

    Add files via upload

--- module_flow.f90
+++ module_flow.f90
@@ -5,16 +5,38 @@ module mod_flow
 
     implicit none
 
+!   Module declarations
+!   Allocatable arrays
     real ( dp ), allocatable :: u(:)
     real ( dp ), allocatable :: p(:)
     real ( dp ), allocatable :: x(:)
     real ( dp ), allocatable :: dens(:)
+    real ( dp ), allocatable :: a(:)
+    real ( dp ), allocatable :: q(:)
 
-    real ( dp ) :: x4
-    real ( dp ) :: u4
-    real ( dp ) :: p4
+    integer :: n
 
-    integer     :: n
+
+!   Initial conditions
+!
+!   Units: k
+    real ( dp ) :: ti = 3330.0_dp
+
+!   Units: N/m**2 * (10E+05)
+    real ( dp ) :: pinit = 690E+5/1.0E+05
+
+
+
+!   Physical constants
+!
+!   Units: N/A
+    real ( dp ), parameter :: gammagas = 1.20_dp
+
+!   Units: J/(kg-k)
+    real ( dp ), parameter :: rgas = 320.0_dp
+
+
+
 
     contains
 
@@ -39,7 +61,7 @@ module mod_flow
         implicit none
 
         n = 3
-        call allocatearray1d ( n )
+        call allocatearray1d ( )
 
 !       Units: m
         x = (/ 0.1000_dp, 0.1250_dp, 0.1500_dp /)
@@ -47,22 +69,10 @@ module mod_flow
 !       Units: m/s
         u = (/ 188.30_dp, 235.30_dp, 282.50_dp /)
 
-!       Units: N*m**2 * (10**5)
+!       Units: N*m**2 * (10E+05)
         p = (/ 111.77_dp, 110.78_dp, 109.54_dp /)
 
-
-        x4 = 0.10180_dp
-
-        call lin1dinterp ( x(1), x(2), u(1), u(2), x4, u4 )
-        call lin1dinterp ( x(1), x(2), p(1), p(2), x4, p4 )
-
-        write(*,*) " X4 = ", x4
-        write(*,*) " U4 = ", u4
-        write(*,*) " P4 = ", p4
-
-
-
-
+        call interiorpoint ( 1 )
 
         call writedata ( )
 
@@ -70,14 +80,14 @@ module mod_flow
 
 
 
-    subroutine allocatearray1d ( n )
+    subroutine allocatearray1d ( )
 ! -------------------------------------------------------------------------- 80
 ! Name: allocatearray1d
 !
 ! Abstract: Allocates array of n-elements to zero
 !
 ! Inputs:
-!   n, integer,
+!   N/A
 !
 ! Outputs:
 !   N/A
@@ -91,9 +101,6 @@ module mod_flow
 ! -------------------------------------------------------------------------- 80
         implicit none
 
-!       Argument declaration
-        integer, intent( in ) :: n
-
 !       Local declaration
         integer :: echo
 
@@ -102,10 +109,14 @@ module mod_flow
         allocate ( p(n) )
         allocate ( x(n) )
         allocate ( dens(n) )
+        allocate ( a(n) )
+        allocate ( q(n) )
         u(1:n) = 0.0_dp
         p(1:n) = 0.0_dp
         x(1:n) = 0.0_dp
         dens(1:n) = 0.0_dp
+        a(1:n) = 0.0_dp
+        q(1:n) = 0.0_dp
 
         echo = 0
 
@@ -125,6 +136,218 @@ module mod_flow
     end subroutine allocatearray1d
 
 
+    subroutine interiorpoint ( i )
+! -------------------------------------------------------------------------- 80
+! Name: interiorpoint
+!
+! Abstract: Perform calculations for points that are not on the boundary
+!
+! Inputs:
+!   i, integer, represents i-th node
+!
+! Outputs:
+!   N/A
+!
+!
+! Description:
+!
+!	This subroutine performs the required calculations to determine the
+!   propagation of the wave on interior (non-boundary) nodes.
+!
+! -------------------------------------------------------------------------- 80
+
+        implicit none
+
+!       Argument declaration
+        integer, intent ( in ) :: i
+
+!       Local declaration
+        real ( dp ) :: lambda_plus
+        real ( dp ) :: lambda_minus
+        real ( dp ) :: x1
+        real ( dp ) :: u1
+        real ( dp ) :: p1
+
+!       Time step
+!       Units: milli-second
+        real ( dp ), parameter :: dt = 0.020_dp
+
+
+!       Calculate speed of sound at node-i
+        call calcspeedofsound ( p(i), a(i) )
+
+!       Calculate lambda plus using the value of u(i) and a(i)
+!       Units: seconds/meter
+        lambda_plus = 1.0_dp / ( u(i) + a(i) )
+
+!       Convert from seconds/meter to milli-seconds/meter
+        lambda_plus = lambda_plus * (1E+03)
+
+!       Find x1 using equation 13.59 from Gas Dynamics Vol. 1, Zucrow & Hoffman
+        x1 = x(i+1) - dt/lambda_plus
+
+!       Find new values of u and p at x1
+        call lin1dinterp ( x(i), x(i+1), u(i), u(i+1), x1, u1)
+        call lin1dinterp ( x(i), x(i+1), p(i), p(i+1), x1, p1)
+
+!       calculate the density using p1
+        call calcdensity ( p1, dens(i) )
+
+!       Calculate the Acoustic impedance
+        call calcq ( i )
+
+
+
+
+
+
+    end subroutine interiorpoint
+
+
+    subroutine calcspeedofsound ( plocal, alocal )
+! -------------------------------------------------------------------------- 80
+! Name: calcspeedofsound
+!
+! Abstract: Calculate speed of sound
+!
+! Inputs:
+!   p, real, the pressure of the fluid
+!            units: N/m**2
+!
+! Outputs:
+!   a, real, the speed of sound
+!            units: m/s**2
+!
+! Description:
+!
+!	This subroutine calculates the speed of sound of the fluid for a given
+!   pressure using equation 13.30 from Gas Dynamics Volume 1, Zucrow & Hoffman
+!
+! -------------------------------------------------------------------------- 80
+        implicit none
+
+!       Argument declaration
+        real ( dp ), intent ( in ) :: plocal
+        real ( dp ), intent ( inout ) :: alocal
+
+!       Local declaration
+        integer :: echo
+
+!       Formula 13.30 (a) from Gas Dynamics Volume 1, Zucrow & Hoffman
+        alocal = sqrt( gammagas * rgas * ti ) * ( plocal / pinit )**(( gammagas - 1.0_dp )/( 2.0_dp * gammagas ))
+
+        echo = 0
+        if ( echo == 1 ) then
+
+            write(*,*) " PLOCAL       = ", plocal
+            write(*,*) " GAMMA GAS    = ", gammagas
+            write(*,*) " R GAS        = ", rgas
+            write(*,*) " INITIAL T    = ", ti
+            write(*,*) " INITIAL P    = ", pinit
+            write(*,*) " "
+            write(*,*) " ALOCAL = ", alocal
+
+        end if
+
+
+
+
+
+    end subroutine calcspeedofsound
+
+
+    subroutine calcdensity ( plocal, denslocal)
+! -------------------------------------------------------------------------- 80
+! Name: calcdensity
+!
+! Abstract: Calculate the fluid density
+!
+! Inputs:
+!   p, real, the pressure of the fluid
+!            units: N/m**2
+!
+! Outputs:
+!   a, real, the fluid density
+!            units: kg/m**3
+!
+! Description:
+!
+!	This subroutine calculates the fluid density using the local pressure
+!   using equation 13.29 from Gas Dynamics Volume 1, Zucrow & Hoffman
+!
+! -------------------------------------------------------------------------- 80
+        implicit none
+
+!       Argument declaration
+        real ( dp ), intent ( in ) :: plocal
+        real ( dp ), intent ( inout ) :: denslocal
+
+!       Local declaration
+        integer :: echo
+
+!       Formula 13.29 (a) from Gas Dynamics Volume 1, Zucrow & Hoffman
+        denslocal = pinit*(1E+5)/( rgas * ti )*( plocal / pinit )**( 1.0_dp / gammagas )
+
+        echo = 0
+        if ( echo == 1 ) then
+
+            write(*,*) " PLOCAL       = ", plocal
+            write(*,*) " GAMMA GAS    = ", gammagas
+            write(*,*) " R GAS        = ", rgas
+            write(*,*) " INITIAL T    = ", ti
+            write(*,*) " INITIAL P    = ", pinit
+            write(*,*) " "
+            write(*,*) " DENS. LOCAL = ", denslocal
+
+        end if
+
+    end subroutine calcdensity
+
+
+    subroutine calcq ( i )
+! -------------------------------------------------------------------------- 80
+! Name: calcq
+!
+! Abstract: Calculate the acoustic impedance
+!
+! Inputs:
+!   i, integer
+!
+! Outputs:
+!   N/A
+!
+!
+! Description:
+!
+!	This subroutine calculates the acoustic impedance
+!   using equation 13.42 from Gas Dynamics Volume 1, Zucrow & Hoffman
+!
+! -------------------------------------------------------------------------- 80
+        implicit none
+
+!       Argument declaration
+        integer, intent ( in ) :: i
+
+!       Local declaration
+        integer :: echo
+
+!       Formula 13.42 (a) from Gas Dynamics Volume 1, Zucrow & Hoffman
+        q(i) = dens(i) * a(i)
+
+        echo = 1
+        if ( echo == 1 ) then
+
+            write(*,*) " DENSITY        = ", dens(i)
+            write(*,*) " SPEED OF SOUND = ", a(i)
+            write(*,*) " "
+            write(*,*) " Q = ", q(i)
+
+        end if
+
+    end subroutine calcq
+
+
+
 
 
     subroutine writedata ( )
@@ -142,11 +365,12 @@ module mod_flow
 !
 ! Description:
 !
-!	Write ASCII data to local machine. !
+!	Write ASCII data to local machine.
 !
 ! -------------------------------------------------------------------------- 80
         implicit none
 
+!       Local declaration
         integer               :: i
         character ( len = 9 ) :: format1
 
@@ -155,6 +379,7 @@ module mod_flow
         open ( unit = 20, file = "output_position.out",    status = "replace" )
         open ( unit = 21, file = "output_velocity.out",    status = "replace" )
         open ( unit = 22, file = "output_pressure.out",    status = "replace" )
+        open ( unit = 23, file = "output_spdsound.out",    status = "replace" )
 
         format1 = "(g14.6)"
 
@@ -163,6 +388,7 @@ module mod_flow
             write ( 20, format1 ) x(i)
             write ( 21, format1 ) u(i)
             write ( 22, format1 ) p(i)
+            write ( 23, format1 ) a(i)
 
         end do
 
@@ -171,9 +397,11 @@ module mod_flow
         close ( 20 )
         close ( 21 )
         close ( 22 )
+        close ( 23 )
 
 
     end subroutine writedata
+
 
 
 
