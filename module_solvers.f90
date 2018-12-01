5c42a81339f19a8242393398dc8074904825a5dc
jens <jerald.daniel@gmail.com>
2018-12-01 12:38:31 -0500

    Update module_solvers.f90

--- module_solvers.f90
+++ module_solvers.f90
@@ -72,6 +72,81 @@ module mod_solvers
         end if
 
     end subroutine lin1dinterp
+    
+    
+            subroutine sor ( a, b, x, n )
+! -------------------------------------------------------------------------- 80
+!       This subroutine is for the Successive over-relaxation method
+!       The algorithm is taken from: Numerical Methods for Engineers
+!       and Scientists by Joe D. Hoffman
+!       See Section 1.7.4
+! -------------------------------------------------------------------------- 80
+
+
+            implicit none
+            ! Subroutine input/output variable declarations
+            integer ( kind = 4 ), intent(in) :: n 						! n is the matrix dimension
+            real    ( kind = 8 ), intent(in) :: a(n,n)						! a is the input matrix
+            real    ( kind = 8 ), intent(in) :: b(n)						! b is the constraints
+            real    ( kind = 8 ), intent(inout) :: x(n)						! x is the solution array
+
+            ! Local variable declarations
+            integer ( kind = 4 ) :: i
+            integer ( kind = 4 ) :: j
+            integer ( kind = 4 ) :: k
+
+            ! w is the relaxation factor. If A is symmetric and positive-definte,
+            ! then the following is needed for convergence: 0 < w < 2
+            ! if w = 1.0, the SOR method becomes Gauss-Seidel
+            real    ( kind = 8 ) :: w
+
+
+            real    ( kind = 8 ) :: sum1(n)
+            real    ( kind = 8 ) :: tolerance
+            real    ( kind = 8 ) :: eps
+            real    ( kind = 8 ) :: old
+            real    ( kind = 8 ) :: new
+
+
+            ! Initializations
+            do i=1,n
+               x(i) = 1.0
+               sum1(i) = 1.0
+            end do
+
+            w = 1.05
+            tolerance = 1e-5
+            eps = 1
+            k = 1
+
+            do while (tolerance < eps)
+                old = x(3)
+                do i=1,n
+                    sum1(i) = 0.0
+                    do j=1,n
+                        if (i /= j) then
+                            sum1(i) = sum1(i) + a(i,j)*x(j)
+                        end if
+                    end do
+                    x(i) = x(i) + w*((b(i) - sum1(i))/a(i,i) - x(i))
+        !			x(i) = (1-w)*x(i) + (w/a(i,i))*(b(i) - sum1(i))
+                end do
+                new = x(3)
+                eps = abs(new - old)
+                k = k + 1
+            end do
+
+            10 format (1x, "Number of iterations til convergence: ", i5)
+            20 format (1x, f15.8)
+
+            write(*,10) k
+            print*, ''
+            do i=1,n
+                write(*,20) x(i)
+            end do
+
+            return
+        end Subroutine sor
 
 
 
